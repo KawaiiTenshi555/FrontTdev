e20d792fda315f83822f02bf51a1df94c52ef595
Noah Brohan <noah.brohan@epitech.eu>
2025-01-20 22:46:54 +0100

    display kpi hardcoded
    api connection tomorrow

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,32 +1,85 @@
 import '../assets/Dashboard.css';
 import { BarChart } from "@mui/x-charts/BarChart";
+import { PieChart } from "@mui/x-charts/PieChart";
 export default function Dashboard() {
     return (
         <div id="main">
             <h1>Dashboard</h1>
             <section>
-                <div className="">Produit les plus vendus</div>
-                <div className="">Produit les moins vendus</div>
-                <div className="">Taux de conversion</div>
-                <div className="">Taux de rupture des stocks</div>
-                <div className="">
-                    <h2>Valeur moyenne des paniers</h2>
+                <div className="kpi-div">
+                    <h2>Produit les plus vendus</h2>
                     <BarChart
                         xAxis={[
                             {
                             id: 'barCategories',
-                            data: ['bar A', 'bar B', 'bar C'],
+                            data: ['banane', 'eau', 'lait','brioche'],
                             scaleType: 'band',
                             },
                         ]}
                         series={[
                             {
-                            data: [2, 5, 3],
+                            data: [20, 54, 31, 63],
                             },
                         ]}
                         width={500}
                         height={300}
                     />
+                </div>
+                <div className="kpi-div">
+                    <h2>Produit les moins vendus</h2>
+                    <BarChart
+                        xAxis={[
+                            {
+                            id: 'barCategories',
+                            data: ['mouchoirs', 'savon', 'shampoingdanstagueule', 'zharicovert'],
+                            scaleType: 'band',
+                            },
+                        ]}
+                        series={[
+                            {
+                            data: [4, 8, 3,1],
+                            },
+                        ]}
+                        width={500}
+                        height={300}
+                    />
+                    <p>titi</p>
+                </div>
+                <div className="kpi-div">
+                    <h2>Taux de conversion</h2>
+                    <p className='font-size-3'>30%</p>
+                    <PieChart
+                        series={[
+                            {
+                            data: [
+                                { id: 0, value: 30, label: 'convertis' },
+                                { id: 1, value: 70, label: 'non convertis' },
+                            ],
+                            },
+                        ]}
+                        width={400}
+                        height={200}
+                    />
+                </div>
+                <div className="kpi-div">
+                    <h2>Taux de rupture des stocks</h2>
+                    <p className='font-size-3'>18%</p>
+                    <PieChart
+                        series={[
+                            {
+                            data: [
+                                { id: 0, value: 18, label: 'plus en stock' },
+                                { id: 1, value: 70, label: 'en stock' },
+                            ],
+                            },
+                        ]}
+                        width={400}
+                        height={200}
+                    />
+                </div>
+                <div className="kpi-div">
+                    <h2>Valeur moyenne des paniers</h2>
+                    <p className='font-size-3'>56.32€</p>
                 </div>
             </section>
         </div>
