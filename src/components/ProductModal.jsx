f15794668fe582ec6921f7b479efb5cd6d2604cf
Matteo Cogan <matteo.cogan@epitech.eu>
2025-01-28 13:10:59 +0000

    Products link

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -3,20 +3,41 @@ import React, { useState, useEffect } from "react";
 const ProductModal = ({ isOpen, onClose, product, onSave }) => {
   const [formData, setFormData] = useState({
     name: "",
-    description: "",
-    warehouse: "",
-    status: "",
+    picture: "",
+    brand: "",
     price: "",
+    type: "",
+    nutritional_information: "",
+    barcode: "",
+    category: "",
   });
+
+  const categories = [
+    { id: 1, name: "boisson" },
+    { id: 2, name: "alimentaire" },
+    { id: 3, name: "snack" },
+    { id: 4, name: "produit laitier" },
+    { id: 5, name: "confiserie" },
+    { id: 6, name: "épicerie" },
+    { id: 7, name: "boulangerie" },
+    { id: 8, name: "pâtisserie" },
+    { id: 9, name: "surgelé" },
+    { id: 10, name: "produit bio" },
+  ];
 
   useEffect(() => {
     if (product) {
+      console.log(product);
+      
       setFormData({
-        name: product.name,
-        description: product.description || "",
-        warehouse: product.warehouse,
-        status: product.status,
-        price: product.price,
+        name: product.name || "",
+        picture: product.picture || "",
+        brand: product.brand || "",
+        price: product.price || "",
+        type: product.type || "",
+        nutritional_information: product.nutritional_information || "",
+        barcode: product.barcode || "",
+        category: product.category.id || "",
       });
     }
   }, [product]);
@@ -25,7 +46,7 @@ const ProductModal = ({ isOpen, onClose, product, onSave }) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: name === "category" ? parseInt(value, 10) : value,
     });
   };
 
@@ -43,71 +64,102 @@ const ProductModal = ({ isOpen, onClose, product, onSave }) => {
           {product ? "Modifier un produit" : "Ajouter un produit"}
         </h2>
         <form onSubmit={handleSubmit}>
-          {/* Champ Nom du produit */}
           <div className="mb-4">
             <input
               type="text"
               name="name"
               value={formData.name}
               onChange={handleInputChange}
-              placeholder="Nom produit"
+              placeholder="Nom du produit"
               className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
               required
             />
           </div>
-
-          {/* Champ Description */}
+          <div className="mb-4">
+            <input
+              type="url"
+              name="picture"
+              value={formData.picture}
+              onChange={handleInputChange}
+              placeholder="URL de l'image"
+              className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+            />
+          </div>
+          <div className="flex gap-4 mb-6">
+            <div className="mb-4">
+              <input
+                type="text"
+                name="brand"
+                value={formData.brand}
+                onChange={handleInputChange}
+                placeholder="Marque"
+                className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="text"
+                name="type"
+                value={formData.type}
+                onChange={handleInputChange}
+                placeholder="Type"
+                className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <input
+                type="number"
+                name="price"
+                value={formData.price}
+                onChange={handleInputChange}
+                placeholder="Prix (€)"
+                className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+                required
+                min="0"
+                step="0.01"
+              />
+            </div>
+          </div>
           <div className="mb-4">
             <textarea
-              name="description"
-              value={formData.description}
+              name="nutritional_information"
+              value={formData.nutritional_information}
               onChange={handleInputChange}
-              placeholder="Description"
+              placeholder="Informations nutritionnelles"
               className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
               rows="2"
+              required
             ></textarea>
           </div>
-
-          {/* Entrepôt, Statut et Prix */}
-          <div className="flex gap-4 mb-6">
-            <select
-              name="warehouse"
-              value={formData.warehouse}
-              onChange={handleInputChange}
-              className="flex-1 p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
-              required
-            >
-              <option value="">Entrepôt</option>
-              <option value="Entrepôt A">Entrepôt A</option>
-              <option value="Entrepôt B">Entrepôt B</option>
-              <option value="Entrepôt C">Entrepôt C</option>
-            </select>
-            <select
-              name="status"
-              value={formData.status}
-              onChange={handleInputChange}
-              className="flex-1 p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
-              required
-            >
-              <option value="">Statut</option>
-              <option value="En stock">En stock</option>
-              <option value="Rupture">Rupture</option>
-              <option value="En commande">En commande</option>
-            </select>
+          <div className="mb-4">
             <input
-              type="number"
-              name="price"
-              value={formData.price}
+              type="text"
+              name="barcode"
+              value={formData.barcode}
               onChange={handleInputChange}
-              placeholder="Prix (€)"
-              className="flex-1 p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+              placeholder="Code-barres"
+              className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
               required
-              min="0"
-              step="0.01"
             />
           </div>
-
-          {/* Boutons */}
+          <div className="mb-4">
+            <select
+              name="category"
+              value={formData.category}
+              onChange={handleInputChange}
+              className="w-full p-3 bg-stone-700 border border-stone-600 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-stone-400"
+              required
+            >
+              <option value="">Sélectionnez une catégorie</option>
+              {categories.map((cat) => (
+                <option key={cat.id} value={cat.id}>
+                  {cat.name}
+                </option>
+              ))}
+            </select>
+          </div>
           <div className="flex justify-between">
             <button
               type="button"
@@ -115,13 +167,15 @@ const ProductModal = ({ isOpen, onClose, product, onSave }) => {
                 onClose();
                 setFormData({
                   name: "",
-                  description: "",
-                  warehouse: "",
-                  status: "",
+                  picture: "",
+                  brand: "",
                   price: "",
+                  type: "",
+                  nutritional_information: "",
+                  barcode: "",
+                  category: "",
                 });
               }}
-              
               className="bg-stone-500 text-white py-2 px-6 rounded-full hover:bg-stone-400"
             >
               Annuler
